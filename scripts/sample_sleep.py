e17fc97d85526ebea4c12c3802883f1f061f3ec2
ayakatakuriko <warangkana0601@yahoo.ne.jp>
2018-12-24 14:06:16 +0900

    fix

--- scripts/sample_sleep.py
+++ scripts/sample_sleep.py
@@ -2,9 +2,9 @@ import time
 import rospy
 
 if __name__ == '__main__':
-    rospy.init_node('sample_sleep')
-    count = 0
-    while not rospy.is_shutdown():
-        time.sleep(5)
-        print "count = " + count
-        count = count + 1
+	rospy.init_node('sample_sleep')
+	count = 0
+	while not rospy.is_shutdown():
+       		time.sleep(5)
+        	rospy.loginfo(count)
+        	count = count + 1
